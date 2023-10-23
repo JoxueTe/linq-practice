9ac78e5642533659716c04cdd5f155507e1d2c41
Josue Tejeda <josuetejeda02@gmail.com>
2023-10-23 10:35:42 -0500

    practicing more linq queries

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+// All the collection
+//PrintValues(queries.GetAllCollection());
+
+//Books after 2000s
+//PrintValues(queries.BooksAfter2000());
+
+//Books with more than 250 pages and action in title
+//PrintValues(queries.BooksMore250PagesAndActionInTitle());
+
+// All books have status
+//Console.WriteLine(queries.BooksPublishedIn2005());
+
+//Books with python category
+PrintValues(queries.BooksContainingPythonCategory());
+
+void PrintValues(IEnumerable<Book> listOfBooks)
+{
+    Console.WriteLine("{0,-55} {1,9} {2,11}\n", "Title", "N. Pages", "Date published");
+    foreach(var item in listOfBooks)
+    {
+        System.Console.WriteLine("{0,-55} {1,9} {2,11}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
